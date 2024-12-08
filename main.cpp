27b8524db9396e0f01070a34bfc333c0eb2e4c7e
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-12-07 20:35:49 -0800

    part 3 done/final commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <list>
 using namespace std;
 
+const int TABLESIZE = 5000;
 
-int sum_ascii(string s);
+int gen_hash_index(string s, int t);
 
 int main() {
     ifstream fin("lab-37-data.txt");
 
     if (fin.is_open()) {
+
+        map<int, list<string>> hash_table;
+
         string codes;
-        int grandtotal = 0;
         while (fin >> codes) {
-            grandtotal += sum_ascii(codes);
+            int index = gen_hash_index(codes, TABLESIZE);
+            hash_table[index].push_back(codes);
         }
 
-        cout << "The total ascii value of the whole file is: " << grandtotal << endl;
+        cout << "First 100 elements displayed:\n";
+        int count = 0;
+        for (const auto& indices : hash_table) {
+            if (count >= 100) {
+                break;
+            }
+            cout << "Table Index: " << indices.first << " | Codes: ";
+            for (const auto& codes : indices.second) {
+                cout << codes << ", ";
+            }
+            cout << endl;
+            count++;
+        }
+        
 
         fin.close();
     }
@@ -26,12 +45,12 @@ int main() {
 
     return 0;
 }
-int sum_ascii(string s) {
+int gen_hash_index (string s, int TABLESIZE) {
     int sum = 0;
     for (char c : s) {
         sum += (int) c;
     }
-    return sum;
+    return sum % TABLESIZE;
 }
 
 
